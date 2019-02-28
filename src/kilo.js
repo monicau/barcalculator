49df3214ecae648a54679b859a483fa7adbbb5d2
Monica Ung <monica.ung@utoronto.ca>
2019-02-28 18:23:33 -0500

    Added plates and value input field

--- src/kilo.js
+++ src/kilo.js
@@ -7,36 +7,41 @@
 
 // console.log(change(d,v).map((v)=>v/10));
 
-export function kiloPlates(denoms, value) {
+export function findKiloPlates (denoms, value, bar) {
+  // Remove decimals by multiplying by 10
+  denoms = denoms.map((x) => x * 10)
+  value = (value - bar) * 10 / 2.0
   // c[p] = minimum # of coins for p cents
-  var c = [];
+  var c = []
   // s[p] = last coin used to make p cents
-  var s = [];
+  var s = []
 
-  for (var p=0; p<=value; p++) {
-    if (p==0) {
-      c[p]=0;
-      s[p]=0;
+  for (var p = 0; p <= value; p++) {
+    if (p === 0) {
+      c[p] = 0
+      s[p] = 0
     } else {
       // min(c[p-d_i]+1) where d1<=d_i<=p
-      var min_c = Number.MAX_SAFE_INTEGER;
-      var min_s = -1;
-      for (var i=0; (i<=denoms.length) && (denoms[i] <= p); i++) {
-        var current_c = c[p-denoms[i]] + 1;
-        if (current_c < min_c) {
-          min_c = current_c;
-          min_s = denoms[i];
+      var minC = Number.MAX_SAFE_INTEGER
+      var minS = -1
+      for (var i = 0; (i <= denoms.length) && (denoms[i] <= p); i++) {
+        var currentC = c[p - denoms[i]] + 1
+        if (currentC < minC) {
+          minC = currentC
+          minS = denoms[i]
         }
       }
-      c[p]=min_c;
-      s[p]=min_s;
+      c[p] = minC
+      s[p] = minS
     }
   }
+  // console.log(c)
+  // console.log(s)
   // List of changes
-  var changes = [];
+  var changes = []
   while (value > 0) {
-    changes.push(s[value]);
-    value = value - s[value];
+    changes.push(s[value] / 10)
+    value = value - s[value]
   }
-  return changes;
+  return changes
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,15 @@
-import React from "react"
-import kiloPlates from "../kilo"
+import React from 'react'
+import { findKiloPlates } from '../kilo'
 
 export default class Index extends React.Component {
-  constructor(props) {
-    super(props);
+  constructor (props) {
+    super(props)
     this.state = {
+      bar: 15,
+      target: 0,
       plates: {
         zeroFive: true,
+        one: true,
         oneFive: true,
         two: true,
         twoFive: true,
@@ -14,44 +17,81 @@ export default class Index extends React.Component {
         ten: true,
         fifteen: true,
         twenty: true,
-        twentyFive: true,
+        twentyFive: true
       }
-    };
+    }
+    this.handleInputChange = this.handleInputChange.bind(this)
   }
-  handleInputChange = (event) => {
-    const target = event.target;
-    const value = target.type === 'checkbox' ? target.checked : target.value;
-    const name = target.name;
 
-    this.setState({
-      plates: {
-        ...this.state.plates,
+  handleInputChange (event) {
+    const target = event.target
+    const value = target.type === 'checkbox' ? target.checked : target.value
+    const name = target.name
+
+    if (target.type === 'checkbox') {
+      this.setState({
+        plates: {
+          ...this.state.plates,
+          [name]: value
+        }
+      })
+    } else {
+      this.setState({
         [name]: value
-      }
-    });
-
+      })
+    }
   }
 
-  render() {
+  renderCheckbox (item) {
+    return <label key={item.id}>{item.label}
+      <input
+        name={item.id}
+        type={'checkbox'}
+        checked={this.state.plates[item.id]}
+        onChange={this.handleInputChange} />
+    </label>
+  }
+  renderRadios (item) {
+    return <label key={item.id}>{item.label}
+      <input
+        name={item.id}
+        type={'radio'}
+        checked={this.state.plates[item.id]}
+        onChange={this.handleInputChange} />
+    </label>
+  }
+
+  render () {
+    var kiloPlates = [
+      { label: '0.5kg', id: 'zeroFive', value: 0.5 },
+      { label: '1kg', id: 'one', value: 1 },
+      { label: '1.5kg', id: 'oneFive', value: 1.5 },
+      { label: '2kg', id: 'two', value: 2 },
+      { label: '2.5kg', id: 'twoFive', value: 2.5 },
+      { label: '5kg', id: 'five', value: 5 },
+      { label: '10kg', id: 'ten', value: 10 },
+      { label: '15kg', id: 'fifteen', value: 15 },
+      { label: '20kg', id: 'twenty', value: 20 },
+      { label: '25kg', id: 'twentyFive', value: 25 }
+    ]
+    var plates = (kiloPlates.filter((item) => this.state.plates[item.id]).map((item) => item.value))
+    var targetPlates = (findKiloPlates(plates, this.state.target, 20))
     return (
       <div>
         <h1>Hello, world!</h1>
-        <label>0.5kg
+
+        { kiloPlates.map((item) => this.renderCheckbox(item)) }
+        <label>
+          Weight
           <input
-            name="zeroFive"
-            type="checkbox"
-            checked={this.state.plates.zeroFive}
-            onChange={this.handleInputChange} />
+            name='target'
+            type='number'
+            value={this.state.target}
+            onChange={this.handleInputChange}
+          />
         </label>
-        <label>1.5kg
-          <input
-            name="oneFive"
-            type="checkbox"
-            checked={this.state.plates.oneFive}
-            onChange={this.handleInputChange} />
-        </label>
-        
+        {targetPlates.map((x) => { return (x + ', ') }) }
       </div>
-    );
+    )
   }
 }
