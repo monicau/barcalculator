adbbd9c0368102c7acaa9963af19e87a6924a293
Monica Ung <monica.ung@utoronto.ca>
2019-06-03 14:47:34 -0400

    Refactor to functional component

--- src/pages/reverseCalculator.js
+++ src/pages/reverseCalculator.js
@@ -1,140 +1,117 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export default class ReverseCalculator extends React.Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-      bar: 20,
-      plates: {
-        kg: { 0.5: 0, 1: 0, 1.5: 0, 2: 0, 2.5: 0, 5: 0, 10: 0, 15: 0, 20: 0, 25: 0 },
-        lb: { 2.5: 0, 5: 0, 10: 0, 25: 0, 35: 0, 45: 0 }
-      }
-    }
-    this.handleInputChange = this.handleInputChange.bind(this)
-    this.reset = this.reset.bind(this)
+const ReverseCalculator = (props) => {
+  const initialPlates = {
+    kg: { 0.5: 0, 1: 0, 1.5: 0, 2: 0, 2.5: 0, 5: 0, 10: 0, 15: 0, 20: 0, 25: 0 },
+    lb: { 2.5: 0, 5: 0, 10: 0, 25: 0, 35: 0, 45: 0 }
   }
+  const kilos = [0.5, 1, 1.5, 2, 2.5, 5, 10, 15, 20, 25]
+  const pounds = [2.5, 5, 10, 25, 35, 45]
+  const [ bar, setBar ] = useState(20)
+  const [ plates, setPlates ] = useState(initialPlates)
 
-  renderPlate (weight, unit) {
+  const renderPlate = (weight, unit) => {
     return (
       <div key={weight + unit}>
         { weight + unit }<br />
-        <button onClick={() => this.handlePlateChange(weight, unit, -1)}>-</button>
-        { this.state.plates[unit][weight]}
-        <button onClick={() => this.handlePlateChange(weight, unit, +1)}>+</button>
+        <button onClick={() => handlePlateChange(weight, unit, -1)}>-</button>
+        { plates[unit][weight]}
+        <button onClick={() => handlePlateChange(weight, unit, +1)}>+</button>
       </div>
     )
   }
 
-  renderSum () {
+  const renderSum = () => {
     var kg = 0
     var lb = 0
-    Object.entries(this.state.plates.kg).forEach(entry => {
+    Object.entries(plates.kg).forEach(entry => {
       const weight = entry[0]
       const count = entry[1]
       kg += weight * count
     })
-    Object.entries(this.state.plates.lb).forEach(entry => {
+    Object.entries(plates.lb).forEach(entry => {
       const weight = entry[0]
       const count = entry[1]
       lb += weight * count
     })
-    const totalKg = this.state.bar + (kg + lb / 2.2046) * 2
-    const totalLb = this.state.bar * 2.2046 + (kg * 2.2046 + lb) * 2
+    const totalKg = bar + (kg + lb / 2.2046) * 2
+    const totalLb = bar * 2.2046 + (kg * 2.2046 + lb) * 2
 
     return <div>
-      { this.round(totalKg, 1) } kg / { this.round(totalLb, 1) } lb
+      { round(totalKg, 1) } kg / { round(totalLb, 1) } lb
     </div>
   }
 
-  reset () {
-    this.setState({
-      ...this.state,
-      plates: {
-        kg: { 0.5: 0, 1: 0, 1.5: 0, 2: 0, 2.5: 0, 5: 0, 10: 0, 15: 0, 20: 0, 25: 0 },
-        lb: { 2.5: 0, 5: 0, 10: 0, 25: 0, 35: 0, 45: 0 }
-      }
-    })
-  }
-
-  round (value, precision) {
+  const round = (value, precision) => {
     var multiplier = Math.pow(10, precision || 0)
     return Math.round(value * multiplier) / multiplier
   }
 
-  handlePlateChange (weight, unit, delta) {
-    const value = this.state.plates[unit][weight] + delta
-    this.setState({
-      plates: {
-        ...this.state.plates,
-        [unit]: {
-          ...this.state.plates[unit],
-          [weight]: value
-        }
+  const handlePlateChange = (weight, unit, delta) => {
+    const value = plates[unit][weight] + delta
+    setPlates({
+      ...plates,
+      [unit]: {
+        ...plates[unit],
+        [weight]: value
       }
     })
   }
 
-  handleInputChange (event) {
+  const handleInputChange = (event) => {
     const target = event.target
     const value = parseFloat(target.value)
-    const name = target.name
-    this.setState({
-      [name]: value
-    })
+    setBar(value)
   }
 
-  render () {
-    const kilos = [0.5, 1, 1.5, 2, 2.5, 5, 10, 15, 20, 25]
-    const pounds = [2.5, 5, 10, 25, 35, 45]
-    return (
-      <div>
-        { this.renderSum() }
-        <button onClick={this.reset}>Reset</button>
-        <br />
-        <label>
-          <input
-            type='radio'
-            name='bar'
-            value={20}
-            checked={this.state.bar === 20}
-            onChange={this.handleInputChange}
-          />
-          20kg
-        </label>
-        <label>
-          <input
-            type='radio'
-            name='bar'
-            value={20.4117}
-            checked={this.state.bar === 20.4117}
-            onChange={this.handleInputChange}
-          />
-          45lb
-        </label>
-        <label>
-          <input
-            type='radio'
-            name='bar'
-            value={15}
-            checked={this.state.bar === 15}
-            onChange={this.handleInputChange}
-          />
-          15kg
-        </label>
-        <label>
-          <input
-            type='radio'
-            name='bar'
-            value={15.8757}
-            checked={this.state.bar === 15.8757}
-            onChange={this.handleInputChange}
-          />
-          35lb
-        </label>
-
-        { kilos.map((x) => this.renderPlate(x, 'kg')) }
-        { pounds.map((x) => this.renderPlate(x, 'lb')) }
-      </div>
-    )
-  }
+  return (
+    <div>
+      { renderSum() }
+      <button onClick={() => setPlates(initialPlates)}>Reset</button>
+      <br />
+      <label>
+        <input
+          type='radio'
+          name='bar'
+          value={20}
+          checked={bar === 20}
+          onChange={handleInputChange}
+        />
+        20kg
+      </label>
+      <label>
+        <input
+          type='radio'
+          name='bar'
+          value={20.4117}
+          checked={bar === 20.4117}
+          onChange={handleInputChange}
+        />
+        45lb
+      </label>
+      <label>
+        <input
+          type='radio'
+          name='bar'
+          value={15}
+          checked={bar === 15}
+          onChange={handleInputChange}
+        />
+        15kg
+      </label>
+      <label>
+        <input
+          type='radio'
+          name='bar'
+          value={15.8757}
+          checked={bar === 15.8757}
+          onChange={handleInputChange}
+        />
+        35lb
+      </label>
+      { kilos.map((x) => renderPlate(x, 'kg')) }
+      { pounds.map((x) => renderPlate(x, 'lb')) }
+    </div>
+  )
 }
+export default ReverseCalculator
