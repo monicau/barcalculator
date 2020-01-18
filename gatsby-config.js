0dadbd2b9a5b8a314b896047c341140319cb297b
Jana Rajakumar <jana.rajakumar@utoronto.ca>
2020-01-17 22:33:00 -0500

    Clean gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,16 +2,7 @@ module.exports = {
   plugins: [
     `gatsby-plugin-sass`,
     {
-      resolve: `gatsby-plugin-material-ui`,
-      // If you want to use styled components, in conjunction to Material-UI, you should: 
-      // - Change the injection order
-      // - Add the plugin
-      options: {
-        // stylesProvider: {
-        //   injectFirst: true,
-        // },
-      }
-      // 'gatsby-plugin-styled-components',
+      resolve: `gatsby-plugin-material-ui`
     },
     {
       resolve: `gatsby-plugin-manifest`,
